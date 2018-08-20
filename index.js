1959a8e53ef2352e8f0ab0dd829955b07183876d
yuanjli <liyuanjienetwork@gmail.com>
2018-08-20 15:44:57 -0700

    Added home page and links

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Require stuff 
+var bodyParser = require('body-parser');
+var ejsLayouts = require('express-ejs-layouts');
 var express = require('express');
 
 
@@ -8,12 +10,20 @@ var app = express();
 
 
 // Set and use statements
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+app.use(bodyParser.urlencoded({extended: false}));  // when you use a form the data you typed in the form can come to the back end. 
+
+
+// Include controllers/routers;
+app.use('/articles', require('./controllers/articles'));
+app.use('/authors', require('./controllers/authors'));  // require indicates that running on the back end.
 
 
 
 // Define routes 
-app.get('/', function(req, res){
-	res.send('home page!');
+app.get('/', function(req, res){   // request and response
+	res.render('home');
 });
 
 
